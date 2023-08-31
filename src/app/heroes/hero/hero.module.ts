a530f6eea459cbbbebbe89d5f8be56a6c49fe056
Seth Arteaga <set007a@gmail.com>
2023-08-30 20:24:59 -0600

    Fin seccion 4

---
+++ src/app/heroes/hero/hero.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { HeroComponent } from "./hero.component";
+import { ListComponent } from "../list/list.component";
+
+
+@NgModule({
+    declarations:[
+        HeroComponent,
+        ListComponent
+    ],
+    exports:[
+        HeroComponent,
+        ListComponent
+    ],
+    imports:[
+        CommonModule
+    ]
+})
+
+export class HeroModule {}
