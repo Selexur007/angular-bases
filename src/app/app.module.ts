3d5795005bea0bf08c502f2d4d7c37c60b7120fa
Seth Arteaga <set007a@gmail.com>
2023-09-11 14:00:38 -0600

    bases angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroModule } from './heroes/hero/hero.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeroModule } from './heroes/hero/hero.module';
     BrowserModule,
     CounterModule,
     HeroModule,
+    DbzModule
     
   ],
   providers: [],
